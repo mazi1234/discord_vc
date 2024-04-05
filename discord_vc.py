f8e5228351cd5fb32cc5128099b18fcb0698154d
mazi1234 <mazintarawa@gmail.com>
2024-04-05 02:12:11 -0400

    Added comments for steps and skeleton for main

---
+++ discord_vc.py
@@ -0,0 +1,16 @@
+"""
+Steps:
+1. Get a log of user joins/leaves using Discord API
+2. Post these join/leave notifications onto a seperate (hidden) text-channel   
+3. Parse the join/leave notifications and get the username, date & time, and game playing (if any)
+4. Send a notification with the information from step 3 to SMS
+"""
+
+
+def main():
+    print("Hello world")
+
+
+
+if __name__ == "__main__":
+    main()
